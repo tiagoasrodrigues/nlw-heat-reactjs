5bd7f079c5684e3f8b1408f184406c5b84f37dd7
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-10-21 18:23:44 -0300

    :wrench: chore: finishing context

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth';
 
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const { signInUrl, user } = useContext(AuthContext);
+
+  console.log(user)
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href='#' className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com GitHub
       </a>
