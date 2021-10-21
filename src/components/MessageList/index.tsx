688ffeac5d401d5f8a3d9a030835c188736ac4f5
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-10-21 16:36:57 -0300

    :wrench: chore: finishing MessageList with nodejs

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,45 +1,47 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg'
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data);
+    })
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-            <div className={styles.messageUser}>
-              <div className={styles.userImage}>
-                <img src="https://github.com/tiagoasrodrigues.png" alt="Tiago Augusto" />
-              </div>
-              <span>Tiago Augusto</span>
-            </div>
-          </p>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Esse vai ser simplesmente fantástico! Bora aprender tudo em relação a montagem de APIs GraphQL. Sem contar com as palestras e painéis.            <div className={styles.messageUser}>
-              <div className={styles.userImage}>
-                <img src="https://github.com/tiagoasrodrigues.png" alt="Tiago Augusto" />
-              </div>
-              <span>Tiago Augusto</span>
-            </div>
-          </p>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Sem dúvida as palestras vão ser úteis para a minha carreira e para a de muitos 😍 #gorocket            <div className={styles.messageUser}>
-              <div className={styles.userImage}>
-                <img src="https://github.com/tiagoasrodrigues.png" alt="Tiago Augusto" />
-              </div>
-              <span>Tiago Augusto</span>
-            </div>
-          </p>
-        </li>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>
+                {message.text}
+                <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                    <img src={message.user.avatar_url} alt={message.user.name} />
+                  </div>
+                  <span>{message.user.name}</span>
+                </div>
+              </p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   )
